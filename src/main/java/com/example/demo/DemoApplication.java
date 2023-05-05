3ada9e251754830cca5e7eaf3c7c9e98f83ee980
Atila <atila.kaya@gmx.de>
2023-05-05 16:50:56 +0200

    Tidying up

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,5 @@
 package com.example.demo;
 
-import com.example.demo.jpa.Customer;
-import com.example.demo.jpa.CustomerRepository;
 import com.example.demo.jpa.EGGUser;
 import com.example.demo.jpa.EGGUserRepository;
 import org.springframework.boot.SpringApplication;
@@ -25,44 +23,14 @@ public class DemoApplication {
 		return (args) -> {
 
 			initializeDB(userRepository);
-
-			// save a few customers
-//			repository.save(new Customer("Jack", "Bauer"));
-//			repository.save(new Customer("Chloe", "O'Brian"));
-//			repository.save(new Customer("Kim", "Bauer"));
-//			repository.save(new Customer("David", "Palmer"));
-//			repository.save(new Customer("Michelle", "Dessler"));
-
-			// fetch all customers
-//			log.info("Customers found with findAll():");
-//			log.info("-------------------------------");
-//			for (Customer customer : repository.findAll()) {
-//				log.info(customer.toString());
-//			}
-//			log.info("");
-
-			// fetch an individual customer by ID
-//			Customer customer = repository.findById(102L);
-//			log.info("Customer found with findById(102L):");
-//			log.info("--------------------------------");
-//			log.info(customer.toString());
-//			log.info("");
-
-			// fetch customers by last name
-//			log.info("Customer found with findByLastName('Bauer'):");
-//			log.info("--------------------------------------------");
-//			repository.findByLastName("Bauer").forEach(bauer -> {
-//				log.info(bauer.toString());
-//			});
-			// for (Customer bauer : repository.findByLastName("Bauer")) {
-			//  log.info(bauer.toString());
-			// }
-			log.info("");
+			log.info("Finished");
 		};
 	}
 
 	private void initializeDB (EGGUserRepository userRepository) {
+		log.info("Delete all users");
 		userRepository.deleteAll();
+		log.info("-------------------------------");
 		userRepository.save(new EGGUser("Jörn", "Schmidt"));
 		userRepository.save(new EGGUser("Max", "Mustermann"));
 		userRepository.save(new EGGUser("Daniel", "Müller"));
